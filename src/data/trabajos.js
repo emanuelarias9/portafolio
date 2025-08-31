8152ee82986e98efc80ecaf7c68138eeb3ea79e3
Emanuel Arias <Emanuelarias9@gmail.com>
2025-08-30 22:56:30 -0500

    Add: include Api VibeShare project

--- src/data/trabajos.js
+++ src/data/trabajos.js
@@ -1,11 +1,22 @@
 export const trabajos = [
   {
+    id: "ApiVibeShare",
+    nombre: "Api VibeShare",
+    urlGithub: "https://github.com/emanuelarias9/ApiVibeShare",
+    urlDeploy: " https://apivibeshare.onrender.com/api-docs",
+    tecnologias:
+      "NodeJS, ExpressJS, MongoDB + Mongoose, Multer, JWT, bcrypt, Swagger",
+    categoria: "Desarrollo Backend",
+    descripcion:
+      "ApiVibeShare es una API REST que soporta funcionalidades clave de una red social: registro y autenticación de usuarios, perfiles, sistema de “follows”, posts y gestión de imágenes. La API está versionada (/api/v1) y documentada con OpenAPI/Swagger.",
+  },
+  {
     id: "Blog",
     nombre: "Blog",
     urlGithub: "https://github.com/emanuelarias9/Blog",
     urlDeploy: "https://blog-esv7.onrender.com",
     tecnologias: "HTML, CSS, React + Vite, React Router DOM, EmailJS, Api REST",
-    categoria: "Desarrollo web",
+    categoria: "Desarrollo Frontend",
     descripcion:
       "Este proyecto es un Blog desarrollado en React que consume una API REST construida en NodeJS Los usuarios pueden crear, ver, editar y eliminar artículos, buscar por palabra clave, y navegar entre distintas páginas como inicio, contacto y listado de publicaciones.",
   },
@@ -14,8 +25,8 @@ export const trabajos = [
     nombre: "API REST Node.js",
     urlGithub: "https://github.com/emanuelarias9/Api-rest-node",
     urlDeploy: "https://api-blog-b0fd.onrender.com/api-docs/",
-    tecnologias: "NodeJS, ExpressJS, MongoDB, Mongoose, Multer, Swagger UI",
-    categoria: "Desarrollo web",
+    tecnologias: "NodeJS, ExpressJS, MongoDB, Mongoose, Multer, Swagger",
+    categoria: "Desarrollo Backend",
     descripcion:
       "API RESTful para la gestión de artículos de un blog, desarrollada con Node.js, Express.js, MongoDB y Mongoose. Permite operaciones CRUD completas, búsqueda avanzada y gestión de imágenes, todo documentado con Swagger/OpenAPI.",
   },
@@ -24,7 +35,7 @@ export const trabajos = [
     nombre: "Syncro",
     urlGithub: "https://github.com/emanuelarias9/Syncro",
     tecnologias: ".NET Maui, C#",
-    categoria: "Desarrollo movil",
+    categoria: "Desarrollo Movil",
     descripcion:
       "Syncro es un Proyecto creado para tener una previsualizacion del daño que haria el hechizo de syncro del xelor basado en el nivel del hechizo y las resistencias del enemigo",
   },
@@ -33,7 +44,7 @@ export const trabajos = [
     nombre: "API REST .NET Core",
     urlGithub: "https://github.com/emanuelarias9/ApiRestNetCore",
     tecnologias: ".NET 8, C#, Entity Framework, Swagger UI",
-    categoria: "Desarrollo web",
+    categoria: "Desarrollo Backend",
     descripcion:
       "Este proyecto es una API RESTful desarrollada con ASP.NET Core 8 y Entity Framework. Su objetivo principal es servir como práctica para comprender los conceptos fundamentales de Entity Framework y su integración en aplicaciones .NET modernas.",
   },
